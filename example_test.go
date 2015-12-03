5956becc3e557ee8e941a47bce9180ca7f5922e1
chai2010 <chaishushan@gmail.com>
2015-12-03 17:47:51 +0800

    more example

--- example_test.go
+++ example_test.go
@@ -5,7 +5,10 @@
 package errors_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"reflect"
 
 	"github.com/chai2010/errors"
 )
@@ -25,6 +28,16 @@ func Example() {
 	// err1 -> {err0}
 	// err2 -> {err1 -> {err0}}
 	// err3 -> {err2 -> {err1 -> {err0}}}
+}
+
+func Example_code() {
+	err := errors.NewWithCode(404, "http error code")
+
+	fmt.Println(err)
+	fmt.Println(err.(errors.Error).Code())
+	// Output:
+	// http error code
+	// 404
 }
 
 func Example_caller() {
@@ -86,4 +99,25 @@ func Example_wraped() {
 	//     caller:2: testing.RunExamples
 	//     caller:3: testing.(*M).Run
 	//     caller:4: main.main
+}
+
+func Example_json() {
+	err0 := errors.New("err0")
+	err1 := errors.Wrap(err0, "err1")
+	err2 := func() error { return errors.Wrap(err1, "err2") }()
+	err3 := func() error { return errors.Wrap(err2, "err3") }()
+
+	err3JsonData, err := json.Marshal(err3)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	errx := errors.NewFromJson(string(err3JsonData))
+	if !reflect.DeepEqual(errx, err3) {
+		log.Fatal(errx, "!=", err3)
+	}
+
+	fmt.Println("done")
+	// Output:
+	// done
 }
