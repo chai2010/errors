08a6919157a47b6819c4603e1d06537c68b916e2
chai2010 <chaishushan@gmail.com>
2015-12-05 21:52:39 +0800

    more test

--- erors_test.go
+++ erors_test.go
@@ -6,11 +6,21 @@ package errors
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
 func TestError(t *testing.T) {
-	//
+	err0 := New("err0 message")
+	tAssert(t, "err0 message" == err0.Error())
+	tAssert(t, len(err0.(Error).Wraped()) == 0)
+
+	err1 := Wrap(err0, "err1 message")
+	tAssert(t, strings.HasPrefix(err1.Error(), "err1 message"))
+	tAssert(t, strings.HasSuffix(err1.Error(), "{"+err0.Error()+"}"))
+	tAssert(t, strings.Contains(err1.Error(), err0.Error()))
+	tAssert(t, len(err1.(Error).Wraped()) == 1)
+	tAssert(t, err1.(Error).Wraped()[0] == err0)
 }
 
 func TestError_callar(t *testing.T) {
